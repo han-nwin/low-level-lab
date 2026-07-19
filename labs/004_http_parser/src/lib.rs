e02d9d1317a6394f1176172b7fe78189686d94f8
han-nwin <hannguyen.win@gmail.com>
2026-07-18 22:20:16 -0500

    lab 4 part 1

--- labs/004_http_parser/src/lib.rs
+++ labs/004_http_parser/src/lib.rs
@@ -1,7 +1,152 @@
-pub struct Parser {}
+// POST /users?id=42&active=true HTTP/1.1
+// Host: localhost:7878
+// User-Agent: curl/8.7.1
+// Accept: application/json
+// Content-Type: application/json
+// Content-Length: 30
+// Connection: close
+//
+// {"name":"Han","role":"admin"}
+//
+//
+// Request {
+//     method: Method::Post,
+//     path: "/users",
+//     query: Some("id=42&active=true"),
+//     version: Version::Http11,
+//     headers: vec![
+//         Header {
+//             name: "Host",
+//             value: "localhost:7878",
+//         },
+//         Header {
+//             name: "User-Agent",
+//             value: "curl/8.7.1",
+//         },
+//         Header {
+//             name: "Accept",
+//             value: "application/json",
+//         },
+//         Header {
+//             name: "Content-Type",
+//             value: "application/json",
+//         },
+//         Header {
+//             name: "Content-Length",
+//             value: "30",
+//         },
+//         Header {
+//             name: "Connection",
+//             value: "close",
+//         },
+//     ],
+//     body: Some(b"{\"name\":\"Han\",\"role\":\"admin\"}"),
+// }
 
-impl Parser {
-    pub fn parse(raw_request: Vec<String>) -> Parser {
-        Parser {}
+// assert_eq!(
+//     b"{\"name\":\"Han\"}",
+//     br#"{"name":"Han"}"#
+// );
+enum Method {
+    POST,
+    GET,
+}
+
+enum Version {
+    HTTP1_0,
+    HTTP1_1,
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    MissingCRLF,
+    InvalidRequest,
+    InvalidMethod,
+    InvalidTarget,
+    InvalidVersion,
+    InvalidHeader,
+}
+
+struct Header {
+    name: String,
+    value: String,
+}
+
+pub struct Request<'a> {
+    pub method: Method,
+    pub path: &'a str,
+    pub query: Option<&'a str>,
+    pub version: Version,
+    pub headers: Vec<Header>,
+    pub body: Option<&'a [u8]>, // byte array -> represent byte string of the body
+}
+
+impl<'a> Request<'a> {
+    pub fn parse(raw_request_bytes: &'a [u8]) -> Result<Request, ParseError> {
+        let mut remaining_bytes = raw_request_bytes;
+
+        let line_end = remaining_bytes
+            .windows(2) // similar to .iter() but do 2 elements at a time
+            .position(|pair| pair == b"\r\n")
+            .ok_or(ParseError::MissingCRLF)?;
+
+        let request_line = &raw_request_bytes[..line_end];
+
+        // 1. Request line
+        let request_line_words: Vec<_> = request_line
+            .split(|byte| byte.is_ascii_whitespace())
+            .collect();
+
+        // 1.1 method
+        // > Match the slice as exactly three elements. If it
+        // > doesn’t match, execute else and leave the
+        // > function.
+        let [method_bytes, target_bytes, version_bytes] = request_line_words.as_slice() else {
+            return Err(ParseError::InvalidRequest);
+        };
+
+        let method_str = str::from_utf8(method_bytes).map_err(|_| ParseError::InvalidMethod)?;
+        let method = match method_str {
+            "GET" => Method::GET,
+            "POST" => Method::POST,
+            _ => return Err(ParseError::InvalidMethod),
+        };
+
+        let version_str = str::from_utf8(version_bytes).map_err(|_| ParseError::InvalidVersion)?;
+        let version = match version_str {
+            "HTTP/1.0" => Version::HTTP1_0,
+            "HTTP/1.1" => Version::HTTP1_1,
+            _ => return Err(ParseError::InvalidVersion),
+        };
+
+        // split at most 2
+        let mut target_iter = target_bytes.splitn(2, |byte| *byte == 63);
+        let path =
+            str::from_utf8(target_iter.next().unwrap()).map_err(|_| ParseError::InvalidTarget)?;
+
+        //  The type changes happen like this:
+        // Option<&[u8]> (from iter.next())
+        //     // .map(str::from_utf8)
+        // Option<Result<&str, Utf8Error>>
+        //     // .transpose()
+        // Result<Option<&str>, Utf8Error>
+        //     // .map_err(...)?
+        // Option<&str>
+        let query: Option<&str> = target_iter
+            .next()
+            .map(str::from_utf8)
+            .transpose()
+            .map_err(|_| ParseError::InvalidTarget)?;
+
+        // TODO: 2. header
+        // TODO: 3. body
+        Ok(Request {
+            method,
+            path,
+            query,
+            version,
+            headers: vec![],
+            body: None,
+        })
     }
 }
--- labs/004_http_parser/src/main.rs
+++ labs/004_http_parser/src/main.rs
@@ -5,13 +5,10 @@
 // -H 'Content-Type: application/json' \
 // -H 'Connection: close' \
 // --data '{"name":"Han","role":"admin"}'
-use http_parser::Parser
+use http_parser::Request;
 use std::{
-    fs,
     io::{BufReader, prelude::*},
-    net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream},
-    thread,
-    time::Duration,
+    net::TcpListener,
 };
 
 fn main() {
@@ -23,7 +20,7 @@ fn main() {
 
     // 2. Read the incoming -> an iterator of type TcpStream
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let mut stream = stream.unwrap();
 
         // get peer addr and local addr
         let peer_addr = stream.peer_addr().unwrap();
@@ -31,13 +28,13 @@ fn main() {
         println!("new connection from: {peer_addr} to {local_addr}");
 
         // 3. Read each request as Buffer
-        let buf_reader = BufReader::new(&stream);
+        let mut buffer: [u8; 4096] = [0; 4096]; // buffer of 0s
 
-        let raw_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).collect();
-        println!("raw_request: {:?}", raw_request);
+        let bytes_read = stream.read(&mut buffer).unwrap();
+        println!("Read: {bytes_read} bytes");
+        // compare with wc -c (count bytes)
+        // printf 'GET / HTTP/1.1\r\nHost: 127.0.0.1:9999\r\nUser-Agent: curl/8.17.0\r\nAccept: */*\r\n\r\n' | wc -c
 
-        let parsed_request = Parser::parse(raw_request);
-
-        println!("parsed_request: {}", parsed_request);
+        let parsed_request = Request::parse(&buffer);
     }
 }
