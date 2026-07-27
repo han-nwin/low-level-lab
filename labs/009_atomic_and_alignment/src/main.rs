6e30711e46072ed948f7aad33025ffcdc7d7d913
han-nwin <hannguyen.win@gmail.com>
2026-07-27 13:56:31 -0500

    some code for lab 9

--- labs/009_atomic_and_alignment/src/main.rs
+++ labs/009_atomic_and_alignment/src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use std::sync::{Arc, Mutex}; // Arc = Atomic Rc = Atomic reference-counted
+use std::thread;
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -24,6 +26,11 @@ enum Commands {
     // },
 }
 
+struct MutexCounter {
+    counter_a: Mutex<u64>,
+    counter_b: Mutex<u64>,
+}
+
 fn main() {
     let cli = Cli::parse();
 
@@ -41,6 +48,26 @@ fn main() {
 
 fn run_mutex(iterations: usize) {
     println!("Mutex version");
+
+    let counter = Arc::new(MutexCounter {
+        counter_a: Mutex::new(0),
+        counter_b: Mutex::new(0),
+    });
+
+    let counter_1 = counter.clone();
+    let handle_1 = thread::spawn(move || {
+        let mut a_cnt = counter_1.counter_a.lock().unwrap();
+        *a_cnt += 1; // deref here because mutex implement Deref trait
+    });
+
+    let counter_2 = counter.clone();
+    let handle_2 = thread::spawn(move || {
+        let mut b_cnt = counter_2.counter_b.lock().unwrap();
+        *b_cnt += 1; // deref here because mutex implement Deref trait
+    });
+
+    handle_1.join().unwrap();
+    handle_2.join().unwrap();
 }
 
 fn run_atomic(iterations: usize) {
