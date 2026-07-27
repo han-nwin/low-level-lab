e244852ec60be99145bda37adf50df48f74c9bb6
han-nwin <hannguyen.win@gmail.com>
2026-07-27 15:13:35 -0500

    lab 9 mutex

--- labs/009_atomic_and_alignment/src/main.rs
+++ labs/009_atomic_and_alignment/src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Parser, Subcommand};
 use std::sync::{Arc, Mutex}; // Arc = Atomic Rc = Atomic reference-counted
 use std::thread;
+use std::time::Instant;
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -34,40 +35,51 @@ struct MutexCounter {
 fn main() {
     let cli = Cli::parse();
 
-    if cli.iterations == 0 {
-        eprintln!("iterations must be greater than zero");
-        std::process::exit(1);
-    }
-
     match cli.command {
         Commands::Mutex => run_mutex(cli.iterations),
         Commands::Atomic => run_atomic(cli.iterations),
         Commands::Aligned => run_aligned(cli.iterations),
     }
+
+    if cli.iterations == 0 {
+        eprintln!("iterations must be greater than zero");
+        std::process::exit(1);
+    }
 }
 
 fn run_mutex(iterations: usize) {
-    println!("Mutex version");
+    println!("Mutex version:");
+
+    let time = Instant::now();
 
     let counter = Arc::new(MutexCounter {
         counter_a: Mutex::new(0),
         counter_b: Mutex::new(0),
     });
 
-    let counter_1 = counter.clone();
+    let counter_1 = counter.clone(); // make a atomic reference
     let handle_1 = thread::spawn(move || {
-        let mut a_cnt = counter_1.counter_a.lock().unwrap();
-        *a_cnt += 1; // deref here because mutex implement Deref trait
+        for i in 1..=iterations {
+            let mut a_cnt = counter_1.counter_a.lock().unwrap();
+            *a_cnt += 1; // deref here because mutex implement Deref trait
+        }
     });
 
     let counter_2 = counter.clone();
     let handle_2 = thread::spawn(move || {
-        let mut b_cnt = counter_2.counter_b.lock().unwrap();
-        *b_cnt += 1; // deref here because mutex implement Deref trait
+        for i in 1..=iterations {
+            let mut b_cnt = counter_2.counter_b.lock().unwrap();
+            *b_cnt += 1; // deref here because mutex implement Deref trait
+        }
     });
 
     handle_1.join().unwrap();
     handle_2.join().unwrap();
+
+    println!("Iterations: {iterations}");
+    println!("Counter A: {}", counter.counter_a.lock().unwrap());
+    println!("Counter B: {}", counter.counter_b.lock().unwrap());
+    println!("Elapsed: {} milli seconds", time.elapsed().as_millis());
 }
 
 fn run_atomic(iterations: usize) {
