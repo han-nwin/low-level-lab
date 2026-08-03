e4f55f487220db6a117b0cbd2090b07d3ac4d9cf
han-nwin <hannguyen.win@gmail.com>
2026-08-03 17:00:21 -0500

    route clk_sys to rosc

--- labs/013_pico_raw_clock_xocs_pll_driver/src/main.rs
+++ labs/013_pico_raw_clock_xocs_pll_driver/src/main.rs
@@ -28,6 +28,9 @@ fn main() -> ! {
     // DataSheet: 8.1.2.3
     // NOTE: The setup flow
     // Crystal → XOSC → PLL_SYS → clk_sys → CPU
+
+    // Since at start, CPU may already use PLL
+    // We need to reconfigure from downstream → upstream
     // enable_xosc
     //     ↓
     // clk_sys → clk_ref: Since the cpu may be using clk_syc, redirect it's source to clk_ref
@@ -43,16 +46,77 @@ fn main() -> ! {
     // clk_sys → PLL_SYS
     //     ↓
     // clk_peri → clk_sys
-    const CLOCK_BASE: *mut u32 = 0x4001_0000 as *mut u32;
 
     unsafe {
         //enable xosc and wait for stable
         enable_xosc_stable();
+        route_clk_sys_to_clk_ref();
+        route_clk_ref_to_rosc();
 
         // Reset PLL
         reset_pll();
     }
     loop {}
+}
+
+unsafe fn route_clk_sys_to_clk_ref() {
+    const CLOCK_BASE: u32 = 0x4001_0000_u32;
+    const CLOCK_SYS_CTRL: *mut u32 = (CLOCK_BASE + 0x3c) as *mut u32;
+    const CLOCK_SYS_SELECTED: *const u32 = (CLOCK_BASE + 0x44) as *const u32;
+    const CLOCK_REF_CTRL: *mut u32 = (CLOCK_BASE + 0x30) as *mut u32;
+    const CLOCK_REF_SELECTED: *const u32 = (CLOCK_BASE + 0x38) as *const u32;
+
+    // CLK_SYS_CTRL.SRC is bit 0:
+    // 0 = CLK_REF
+    // 1 = CLK_SYS_AUX
+    const CLOCK_SYS_SRC_VALUE: u32 = 0; // set this to route to clk_ref
+
+    // Route clk sys to clk ref
+    unsafe {
+        let clk_ctr = core::ptr::read_volatile(CLOCK_SYS_CTRL);
+        // unset the last bit with & !0b1 then set it to new value
+        let new_clk_ctr = (clk_ctr & !0b1_u32) | CLOCK_SYS_SRC_VALUE;
+        core::ptr::write_volatile(CLOCK_SYS_CTRL, new_clk_ctr);
+
+        // use CLOCK_SYS_SELECTED to see if switching is completed
+        // 01 → CLOCK_REF
+        // 10 → CLKSRC_CLK_SYS_AUX
+        // 00 → switching in progress
+        // use & 0b11 to ignore the other 30 bit
+        while core::ptr::read_volatile(CLOCK_SYS_SELECTED) & 0b11 != 0b01 {
+            core::hint::spin_loop();
+        }
+    }
+}
+
+unsafe fn route_clk_ref_to_rosc() {
+    const CLOCK_BASE: u32 = 0x4001_0000_u32;
+    const CLOCK_REF_CTRL: *mut u32 = (CLOCK_BASE + 0x30) as *mut u32;
+    const CLOCK_REF_SELECTED: *const u32 = (CLOCK_BASE + 0x38) as *const u32;
+
+    // 0x0 → ROSC_CLKSRC_PH  = 0b00
+    // 0x1 → CLKSRC_CLK_REF_AUX = 0b01
+    // 0x2 → XOSC_CLKSRC = 0b10
+    // 0x3 → LPOSC_CLKSRC = 0b11
+    const CLOCK_REF_SRC_VALUE: u32 = 0b00_u32; // set this to route to ROSC
+
+    // route clk_ref to ROSC
+    unsafe {
+        let clk_ctr = core::ptr::read_volatile(CLOCK_REF_CTRL);
+        // unset the last 2 bit and set it to 0
+        // can just be unset but here for later if we need to use different mask
+        let new_clk_ctr = (clk_ctr & !0b11_u32) | CLOCK_REF_SRC_VALUE;
+        core::ptr::write_volatile(CLOCK_REF_CTRL, new_clk_ctr);
+
+        // use CLOCK_REF_SELECTED to see if switching is completed
+        // 0b0001 → ROSC_CLKSRC_PH
+        // 0b0010 → CLKSRC_CLK_REF_AUX
+        // 0b0100 → XOSC_CLKSRC
+        // 0b1000 → LPOSC_CLKSRC
+        while core::ptr::read_volatile(CLOCK_REF_SELECTED) & 0b1111 != 0b0001 {
+            core::hint::spin_loop();
+        }
+    }
 }
 
 unsafe fn enable_xosc_stable() {
@@ -63,8 +127,14 @@ unsafe fn enable_xosc_stable() {
     const XOSC_STARTUP: *mut u32 = (XOSC_BASE + 0x0c) as *mut u32;
     // Table 599
     const XOSC_STABLE_BIT: u32 = (1 << 31) as u32;
-    const XOSC_ENABLE_BIT: u32 = (0xfab << 12) as u32;
-    const XOSC_FREQ_RANGE: u32 = 0xaa0_u32; // 1_15MHZ
+    // CTRL register
+    // 31        24 23                  12 11                   0
+    // +-----------+----------------------+----------------------+
+    // | Reserved  | ENABLE (12 bits)     | FREQ_RANGE (12 bits) |
+    // +-----------+----------------------+----------------------+
+    //               0xFAB                 0xaa0
+    const XOSC_ENABLE_BIT: u32 = (0xfab << 12) as u32; // has to be from 23-12 bits
+    const XOSC_FREQ_RANGE: u32 = 0xaa0_u32; // 1_15MHZ, bits 0-11
 
     unsafe {
         // Datasheet table 588: CTRL Register
