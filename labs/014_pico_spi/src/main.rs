2ef50410d1adc0608dcb2faefa74f5ae996bd15d
han-nwin <hannguyen.win@gmail.com>
2026-08-05 09:11:25 -0500

    lab 14 setup

--- labs/014_pico_spi/src/main.rs
+++ labs/014_pico_spi/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m as _;
+use cortex_m_rt::entry;
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    loop {}
 }
