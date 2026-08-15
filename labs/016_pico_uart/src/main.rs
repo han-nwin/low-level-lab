233c3709df4ffd8735a776ecaea1517b4bfc1999
han-nwin <hannguyen.win@gmail.com>
2026-08-15 11:01:24 -0500

    lab 16 setup

--- labs/016_pico_uart/src/main.rs
+++ labs/016_pico_uart/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod logging;
+use cortex_m as _;
+use cortex_m_rt::entry;
+use panic_halt as _;
+
+#[used]
+#[unsafe(link_section = ".start_block")]
+static IMAGE_DEF: [u32; 5] = [
+    // this is the RP2350 metadata
+    // in datasheet: 5.9.5.1. Minimum Arm IMAGE_DEF
+    0xffff_ded3, // Start marker: tells the Boot ROM an RP2350 metadata block begins here
+    0x1021_0142, // Image type: executable Secure Arm code for the RP2350
+    0x0000_01ff, // Last-item marker: there are no more metadata items in this block
+    0x0000_0000, // Next-block relative offset: 0 makes this single block link to itself
+    0xab12_3579, // End marker: tells the Boot ROM the metadata block ends here
+];
+
+#[used]
+#[unsafe(link_section = ".my_metadata")]
+static METADATA: [u32; 1] = [u32::from_le_bytes(*b"Han!")];
+
+#[entry]
+fn main() -> ! {
+    logging::init();
+    logln!("Hello, world!");
+    loop {
+        logging::poll();
+        cortex_m::asm::delay(100_000); // poll every 100k cycles
+    }
 }
