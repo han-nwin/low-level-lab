0e1c622d5b5528ffa28c191c87c2f8db4d939525
han-nwin <hannguyen.win@gmail.com>
2026-08-22 18:39:15 -0500

    lab17 set up

---
+++ labs/017_pico_timers/src/logging.rs
@@ -0,0 +1,91 @@
+//! Reusable USB CDC logger for RP2350 labs.
+//!
+//! Call [`init`] once near the start of `main`, then call [`poll`] frequently
+//! (at least every few milliseconds). Use `log!` and `logln!` like `print!`
+//! and `println!`.
+
+use rp_usb_serial::RpUsbConsole;
+use rp235x_hal as hal;
+
+/// Crystal frequency on a Raspberry Pi Pico 2.
+const XTAL_FREQ_HZ: u32 = 12_000_000;
+
+/// Configure the clocks and expose the RP2350 USB peripheral as a CDC serial
+/// port. This intentionally uses the HAL so the labs can stay register-level.
+pub fn init() {
+    // moves the low-level WATCHDOG peripheral handle into a higher-level HAL object
+    let mut peripherals = hal::pac::Peripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(peripherals.WATCHDOG);
+
+    // Configure the chip clock
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        peripherals.XOSC,
+        peripherals.CLOCKS,
+        peripherals.PLL_SYS,
+        peripherals.PLL_USB,
+        &mut peripherals.RESETS,
+        &mut watchdog,
+    )
+    .expect("clock initialization failed");
+
+    // Initialize USB serial console
+    // After this call, the USB controller can present itself to
+    // the computer as a CDC serial device.
+    //
+    // Initialization alone is insufficient because this is a
+    // polled USB implementation. The program repeatedly calls:
+    //
+    // logging::poll();
+    //
+    // at src/main.rs:296. That advances USB communication and
+    // sends buffered log output.
+    RpUsbConsole::init(
+        peripherals.USB,
+        peripherals.USB_DPRAM,
+        &mut peripherals.RESETS,
+        clocks.usb_clock,
+    );
+}
+
+//            logging::poll()
+//                  │
+//        ┌─────────┴─────────┐
+//        ▼                   ▼
+//   Mac → Pico           Pico → Mac
+//        │                   │
+//        ▼                   ▼
+//    RX queue            TX queue
+//        │                   ▲
+//        ▼                   │
+// logging::read()          logln!()
+
+/// Advance USB enumeration and flush buffered log messages.
+#[inline]
+pub fn poll() {
+    RpUsbConsole::poll();
+}
+
+/// Read bytes sent by the host. `poll()` must be called regularly first.
+/// Use to read bytes comming into the MCU
+#[allow(dead_code)]
+pub fn read(buffer: &mut [u8]) -> usize {
+    RpUsbConsole::read(buffer)
+}
+
+//  - $arg is a metavariable.
+// - :tt means one token tree.
+// - $( ... )* means repeat the enclosed pattern zero or more times.
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {
+        rp_usb_serial::usb_print!($($arg)*)
+    };
+}
+
+#[macro_export]
+macro_rules! logln {
+    ($($arg:tt)*) => {
+        rp_usb_serial::usb_println!($($arg)*)
+    };
+}
--- labs/017_pico_timers/src/main.rs
+++ labs/017_pico_timers/src/main.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod logging;
+use cortex_m as _;
+use cortex_m_rt::entry;
+use panic_halt as _;
+
+#[used]
+#[unsafe(link_section = ".start_block")]
+static IMAGE_DEF: [u32; 5] = [
+    // this is the RP2350 metadata
+    // in datasheet: 5.9.5.1. Minimum Arm IMAGE_DEF
+    0xffff_ded3, // Start marker: tells the Boot ROM an RP2350 metadata block begins here
+    0x1021_0142, // Image type: executable Secure Arm code for the RP2350
+    0x0000_01ff, // Last-item marker: there are no more metadata items in this block
+    0x0000_0000, // Next-block relative offset: 0 makes this single block link to itself
+    0xab12_3579, // End marker: tells the Boot ROM the metadata block ends here
+];
+
+#[used]
+#[unsafe(link_section = ".my_metadata")]
+static METADATA: [u32; 1] = [u32::from_le_bytes(*b"Han!")];
+
+// 1. RESET
+const RESETS_BASE: u32 = 0x4002_0000_u32;
+const RESETS_RESET: *mut u32 = (RESETS_BASE) as *mut u32;
+const RESETS_RESET_DONE: *const u32 = (RESETS_BASE + 0x08) as *const u32; // readonly
+const TIMER0_RESET_MASK: u32 = (1 << 23) as u32; // GP0 and GP1
+
+// 2.
+const TIMER0_BASE: u32 = 0x400b_0000_u32;
+
+#[entry]
+fn main() -> ! {
+    logging::init(); // logging and configure clock sys to 150MHZ
+    unsafe {
+        // 1. Reset TIMER0
+        // Reset
+        let reset = core::ptr::read_volatile(RESETS_RESET);
+        core::ptr::write_volatile(RESETS_RESET, reset | TIMER0_RESET_MASK);
+        // Enable
+        let reset = core::ptr::read_volatile(RESETS_RESET);
+        core::ptr::write_volatile(RESETS_RESET, reset & !(TIMER0_RESET_MASK));
+        // Wait
+        let mut time_out = 1_000_000;
+        while core::ptr::read_volatile(RESETS_RESET_DONE) & TIMER0_RESET_MASK != TIMER0_RESET_MASK {
+            time_out -= 1;
+            if time_out <= 0 {
+                panic!("UART1 reset timeout")
+            }
+            core::hint::spin_loop();
+        }
+    }
+
+    loop {
+        logging::poll();
+        cortex_m::asm::delay(100_000); // poll every 100k cycles
+    }
 }
