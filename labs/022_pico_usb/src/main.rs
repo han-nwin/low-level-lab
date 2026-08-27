5e17234ec8391a27138a9ff9c97e05d37416ee69
han-nwin <hannguyen.win@gmail.com>
2026-08-27 17:07:37 -0500

    project start

--- labs/022_pico_usb/src/main.rs
+++ labs/022_pico_usb/src/main.rs
@@ -1,3 +1,17 @@
+// NOTE:
+// USB
+// ---
+// D+ = differential data +
+// D- = differential data -
+// ---
+// Connect:
+// D+ -> D+
+// D- -> D-
+// ---
+// Do NOT cross them.
+// ---
+// Think:
+// USB data travels as a differential pair.
 fn main() {
     println!("Hello, world!");
 }
